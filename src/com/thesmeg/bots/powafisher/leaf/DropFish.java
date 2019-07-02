6fc6fc18d7ab02c19abfa372f857518ad6d60855
judd <justinsr20@gmail.com>
2019-07-02 10:29:21 +1000

    added dropping of anchovies

--- src/com/thesmeg/bots/powafisher/leaf/DropFish.java
+++ src/com/thesmeg/bots/powafisher/leaf/DropFish.java
@@ -9,13 +9,16 @@ import com.runemate.game.api.script.framework.tree.LeafTask;
  * NOTES:
  */
 public class DropFish extends LeafTask {
-//    String[] to_drop = {"Raw shrimps", "Anchovies"};
 
     @Override
     public void execute() {
         SpriteItemQueryResults shrimp_to_drop = Inventory.newQuery().names("Raw shrimps").results();
+        SpriteItemQueryResults anchovies_to_drop = Inventory.newQuery().names("Anchovies").results();
         if (shrimp_to_drop.size() > 0) {
             shrimp_to_drop.first().interact("Drop");
+        }
+        if (anchovies_to_drop.size() > 0) {
+            anchovies_to_drop.first().interact("Drop");
         }
     }
 
