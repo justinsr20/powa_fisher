61907768a6069cdf9d17c916898354eb38dd4552
judd <justinsr20@gmail.com>
2019-07-06 11:42:03 +1000

    added delay on fishing net clicks

--- src/com/thesmeg/bots/powafisher/leaf/Fish.java
+++ src/com/thesmeg/bots/powafisher/leaf/Fish.java
@@ -1,22 +1,27 @@
 package com.thesmeg.bots.powafisher.leaf;
 
+import com.runemate.game.api.hybrid.entities.Npc;
 import com.runemate.game.api.hybrid.entities.Player;
+import com.runemate.game.api.hybrid.local.hud.interfaces.Inventory;
 import com.runemate.game.api.hybrid.region.Npcs;
 import com.runemate.game.api.hybrid.region.Players;
 import com.runemate.game.api.script.framework.tree.LeafTask;
+import com.runemate.game.api.script.Execution;
 
 /**
  * NOTES:
  */
 public class Fish extends LeafTask {
-    private Player player;
 
+    Player p;
+    Npc fishing_spot;
 
     @Override
     public void execute() {
-        int animation_id = Players.getLocal().getAnimationId();
-        if (animation_id == -1) {
-            Npcs.newQuery().names("Fishing spot").actions("Net").results().nearest().click();
+        p = Players.getLocal();
+        fishing_spot = Npcs.newQuery().names("Fishing spot").actions("Net").results().nearest();
+        if (fishing_spot.interact("Net")) {
+            Execution.delayUntil(() -> Inventory.isFull(), () -> p.getAnimationId() != -1, 50, 1000, 2000);
         }
     }
 }
