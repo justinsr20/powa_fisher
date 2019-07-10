41ced4c577698aeaede7d689ccc4452298a1ebc1
judd <justinsr20@gmail.com>
2019-07-10 12:01:32 +1000

    added ordered looping to dropping

--- src/com/thesmeg/bots/powafisher/leaf/DropFish.java
+++ src/com/thesmeg/bots/powafisher/leaf/DropFish.java
@@ -1,11 +1,14 @@
 package com.thesmeg.bots.powafisher.leaf;
 
+import com.runemate.game.api.hybrid.entities.definitions.ItemDefinition;
 import com.runemate.game.api.hybrid.input.Keyboard;
 import com.runemate.game.api.hybrid.local.hud.interfaces.Inventory;
+import com.runemate.game.api.hybrid.local.hud.interfaces.SpriteItem;
 import com.runemate.game.api.hybrid.queries.results.SpriteItemQueryResults;
 import com.runemate.game.api.script.framework.tree.LeafTask;
 
 import java.awt.event.KeyEvent;
+import java.util.List;
 
 
 /**
@@ -15,17 +18,16 @@ public class DropFish extends LeafTask {
 
     @Override
     public void execute() {
-        SpriteItemQueryResults shrimpToDrop = Inventory.newQuery().names("Raw shrimps").results();
-        SpriteItemQueryResults anchoviesToDrop = Inventory.newQuery().names("Raw anchovies").results();
+        List<SpriteItem> listOfItems = Inventory.getItems().asList();
+
         //TODO make sure this setting is enabled otherwise bot will bug out
         Keyboard.pressKey(KeyEvent.VK_SHIFT);
-        if (shrimpToDrop.size() > 0) {
-            shrimpToDrop.first().click();
-        }
-        if (anchoviesToDrop.size() > 0) {
-            anchoviesToDrop.first().click();
-        }
+        listOfItems.forEach(item -> {
+            ItemDefinition itemDefinition = item.getDefinition();
+            if (!itemDefinition.getName().equals("Small fishing net")) {
+                item.click();
+            }
+        });
         Keyboard.releaseKey(KeyEvent.VK_SHIFT);
     }
-
 }
