be0c2489c4dd706fe03ffc2ef60a324fea685640
judd <justinsr20@gmail.com>
2019-08-04 18:49:31 +1000

    changed loot approach

--- src/com/thesmeg/bots/fleshcrawler/branch/LootAvailable.java
+++ src/com/thesmeg/bots/fleshcrawler/branch/LootAvailable.java
@@ -1,22 +1,18 @@
 package com.thesmeg.bots.fleshcrawler.branch;
 
+import com.runemate.game.api.hybrid.location.Area;
+import com.runemate.game.api.hybrid.region.GroundItems;
+import com.runemate.game.api.hybrid.region.Players;
 import com.runemate.game.api.script.framework.tree.BranchTask;
 import com.runemate.game.api.script.framework.tree.TreeTask;
 import com.thesmeg.bots.fleshcrawler.leaf.Fight;
 import com.thesmeg.bots.fleshcrawler.leaf.Loot;
 
-import java.util.Random;
-
 public class LootAvailable extends BranchTask {
-//    Loot loot = new Loot();
-    private Random random = new Random();
 
     @Override
     public boolean validate() {
-        if (random.nextInt(1) == 0) {
-            return true;
-        }
-        return false;
+        return !GroundItems.getLoadedWithin(Area.circular(Players.getLocal().getPosition(), 2)).isEmpty();
     }
 
     @Override
