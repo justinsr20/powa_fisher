f09d90314a8df20f63dd67c9946e2fbd91ba46f4
judd <justinsr20@gmail.com>
2019-08-06 12:13:47 +1000

    changed supplies back as teled as soon as food count dropped

--- src/com/thesmeg/bots/fleshcrawler/branch/HaveSupplies.java
+++ src/com/thesmeg/bots/fleshcrawler/branch/HaveSupplies.java
@@ -3,17 +3,17 @@ package com.thesmeg.bots.fleshcrawler.branch;
 import com.runemate.game.api.hybrid.local.hud.interfaces.Inventory;
 import com.runemate.game.api.script.framework.tree.BranchTask;
 import com.runemate.game.api.script.framework.tree.TreeTask;
-import com.thesmeg.bots.fleshcrawler.leaf.GetSupplies;
 
-import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
 
 public class HaveSupplies extends BranchTask {
-    private GetSupplies getSupplies = new GetSupplies();
+    private List<String> requiredItems = Arrays.asList("Pike", "Fire rune", "Law rune", "Air rune");
 
     @Override
     public boolean validate() {
-        for (Map.Entry<String, Integer> item : getSupplies.requiredItems().entrySet()) {
-            if (!Inventory.contains(item.getKey()) || Inventory.getQuantity(item.getKey()) != item.getValue()) {
+        for (String item : requiredItems) {
+            if (!Inventory.contains(item)) {
                 return false;
             }
         }
